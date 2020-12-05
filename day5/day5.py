6febc0b47ee7abf5e95ac99f94f101ec8c459a49
Francesco Martini <wrCisco@users.noreply.github.com>
2020-12-05 17:13:32 +0100

    Add more concise solution for day 5

--- day5/day5.py
+++ day5/day5.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import re
 
 def calc_pos(code, max_):
     pos = 0
@@ -13,17 +14,23 @@ def run():
     with open('input.txt', encoding='utf-8') as fh:
         data = [line.strip() for line in fh]
 
-    max_id = -1
-    ids = []
-    for line in data:
-        row = calc_pos(line[:7], 128)
-        col = calc_pos(line[7:], 8)
-        id_ = row * 8 + col
-        if id_ > max_id:
-            max_id = id_
-        ids.append(id_)
-    print(max_id)  # first answer
+    # my solution
+    # max_id = -1
+    # ids = []
+    # for line in data:
+    #     row = calc_pos(line[:7], 128)
+    #     col = calc_pos(line[7:], 8)
+    #     id_ = row * 8 + col
+    #     if id_ > max_id:
+    #         max_id = id_
+    #     ids.append(id_)
+    # print(max_id)  # first answer
+
+    # I thought of this one after a hint, many hours later...
+    ids = [int(f"0b{re.sub(r'[FL]', '0', re.sub(r'[BR]', '1', line))}", 2) for line in data]
+    
     ids.sort()
+    print(ids[-1])  # first answer
     for i in range(len(ids) - 1):
         if ids[i] < ids[i+1] - 1:
             print(ids[i] + 1)  # second answer
