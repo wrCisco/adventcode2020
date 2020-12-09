900de3a4ac324cd854cd68436537e83ec2c170b7
Francesco Martini <wrCisco@users.noreply.github.com>
2020-12-09 06:48:00 +0100

    Add solutions day 9

---
+++ day09/day09.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+
+import collections
+
+
+def run():
+    with open('input.txt') as fh:
+        lines = [int(line.strip()) for line in fh]
+
+    for i, num in enumerate(lines[25:], 25):
+        sums = set()
+        for n in lines[i-25:i]:
+            if n in sums:
+                break
+            else:
+                sums.add(num - n)
+        else:
+            print(num)  # first answer
+            break
+
+    target = num
+    contiguous = collections.deque()
+    tot = 0
+    i = 0
+    while i < len(lines):
+        if tot < target:
+            contiguous.append(lines[i])
+            tot += lines[i]
+            i += 1
+        elif tot > target:
+            tot -= contiguous.popleft()
+        else:
+            print(min(contiguous) + max(contiguous))  # second answer
+            break
+
+
+if __name__ == '__main__':
+    run()
