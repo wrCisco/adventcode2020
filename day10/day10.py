9b8f9d25d5ee9c45f38f92deb6630ffec46968f9
Francesco Martini <wrCisco@users.noreply.github.com>
2020-12-10 08:17:50 +0100

    Add simpler day 10 solution

--- day10/day10.py
+++ day10/day10.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import itertools
+import collections
 
 def run():
     with open('input.txt') as fh:
@@ -24,7 +25,7 @@ def run():
     not_expendables = [0]
     s.insert(0, 0)
     for i in range(1, len(s) - 1):
-        if abs(s[i - 1] - s[i + 1]) > 3:
+        if s[i + 1] - s[i - 1] > 3:
             not_expendables.append(i)
     not_expendables.append(len(s) - 1)
     tot = 1
@@ -33,7 +34,17 @@ def run():
         distance = not_expendables[i] - not_expendables[i-1]
         tot *= sum(len(list(itertools.combinations(s[not_expendables[i-1]+1:not_expendables[i]], n))) for n in range(max(0, diff - 3), distance))
     # not really a universal solution, but if differences between consecutive values in original sequence are all 1 and 3, it should hold
-    print(tot)  # second answer
+    print(tot)  # second answer method 1
+
+    # this is something I'd like to have thought of by myself...
+    combinations = collections.defaultdict(int)
+    combinations[0] = 1
+    for i in range(1, len(s)):
+        for j in range(i - 1, -1, -1):
+            if s[i] - s[j] > 3:
+                break
+            combinations[i] += combinations[j]
+    print(combinations[len(s)-1])  # second answer method 2
 
 
 if __name__ == '__main__':
