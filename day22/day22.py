2f051f36d57489a997f69a255608acef7baf98a2
Francesco Martini <wrCisco@users.noreply.github.com>
2020-12-29 09:24:03 +0100

    More compact day 22 solution

--- day22/day22.py
+++ day22/day22.py
@@ -14,33 +14,27 @@ def combat(p1, p2, second, prevs):
         if second and c1 <= len(p1) and c2 <= len(p2):
             p1c = deque(list(p1)[:c1])
             p2c = deque(list(p2)[:c2])
-            winner = combat(p1c, p2c, True, set())
+            # if player1 has the highest card, they're the winner, otherwise play the subgame
+            winner = p1c if max(p1c) > max(p2c) else combat(p1c, p2c, True, set())
             if winner is p1c:
-                p1.append(c1)
-                p1.append(c2)
+                w = p1
             else:
-                p2.append(c2)
-                p2.append(c1)
+                w, c1, c2 = p2, c2, c1
         elif c1 > c2:
-            p1.append(c1)
-            p1.append(c2)
+            w = p1
         else:
-            p2.append(c2)
-            p2.append(c1)
-    return p1 if len(p1) > len(p2) else p2
+            w, c1, c2 = p2, c2, c1
+        w.extend((c1, c2))
+    return p1 if not len(p2) else p2
 
 
 def run():
     with open('input.txt') as fh:
-        players = [deque(), deque()]
-        i = -1
-        for line in fh:
-            if line.startswith('Player'):
-                i += 1
-                continue
-            if line.strip():
-                players[i].append(int(line.strip()))
-    p1, p2 = players[0], players[1]
+        p1, p2 = [
+            deque(
+                map(int, (n for n in seq.split('\n')))
+            ) for seq in fh.read()[10:].split('\n\nPlayer 2:\n')
+        ]
 
     winner = combat(p1.copy(), p2.copy(), False, set())
     print(sum(winner.pop() * i for i in range(1, len(winner) +1)))  # first answer
